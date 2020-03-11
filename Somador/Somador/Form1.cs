1261ba603a3dc41d2e2712272289a00fff9ff4be
Elvis Lopes <34426076+Elvis-Lopes@users.noreply.github.com>
2020-03-10 21:18:06 -0300

    Add files via upload
    Programa que efetua soma de duas variáveis e retorna o resultado em Int ou Doube

---
+++ Somador/Somador/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Somador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox2.Text);
+            double res = num1 + num2;
+            textBox3.Text = res.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(textBox1.Text);
+            int num2 = Int32.Parse(textBox2.Text);
+            int res = num1 + num2;
+            textBox3.Text = res.ToString();
+        }
+    }
+}

2cdf6293982f13add90626730f8f06b04cfc6a2f
Elvis Lopes <34426076+Elvis-Lopes@users.noreply.github.com>
2020-03-31 21:34:06 -0300

    Add files via upload

---
+++ Verificador de caracter/Verificador de caracter/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Verificador_de_caracter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char letra;
+            letra = Convert.ToChar(textBox1.Text);
+            if (Char.IsUpper(letra))
+            {
+                MessageBox.Show("A letra é maiuscula.");
+            }
+            else if (Char.IsLower(letra))
+            {
+                MessageBox.Show("A letra é minuscula.");
+            }
+            else if (Char.IsDigit(letra))
+            {
+                MessageBox.Show("O caractere é um número.");
+            }
+            else
+            {
+                MessageBox.Show("O caractere digitado não é alfanúmerico (especial).");
+            }
+        }
+    }
+}
---
+++ Verificar temperatura/aula 31-03-2020/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula_31_03_2020
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float temp;
+            temp = float.Parse(textBox1.Text);
+            if(temp > 37.7)
+            {
+                MessageBox.Show("Esta com febre!\n" +
+                                "Por favor, aguarde ao lado!\n" +
+                                "Sua compra será assistida!");
+            }
+            else
+            {
+                MessageBox.Show("Pode entrar higienize as mãos com álcool");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
